81eb9785661a452bc524b89ea6855b5979423ef7
Quinn Harris <quinn@qutek.net>
2011-04-02 11:49:58 -0600

    Artwork tag fix

--- app/models/artwork_tag.rb
+++ app/models/artwork_tag.rb
@@ -3,14 +3,9 @@ class ArtworkTag < ActiveRecord::Base
   
   OrderTask
   @@tag_mapping = {
-    'proof' => [ArtPrepairedOrderTask], #, 'supplier'],
-    'supplier' => [ArtSentItemTask], #, 'proof'],
-    'customer' => [nil]
+    'proof' => ArtPrepairedOrderTask,
+    'supplier' => ArtSentItemTask,
+    'customer' => nil
   }
-
-  def self.tags_availible(tags)
-    @@tag_mapping.collect do |name, list|
-      [name, (list[1..-1] & tags).empty? && list.first]
-    end
-  end
+  cattr_accessor :tag_mapping
 end
