5f3f3757ee2d3dea236771dc341da7218ca5355b
Quinn Harris <quinn@qutek.net>
2011-06-25 17:22:12 -0600

    Calls

---
+++ db/migrate/20110611214347_calls.rb
@@ -0,0 +1,32 @@
+class Calls < ActiveRecord::Migration
+  def self.up
+    create_table :calls do |t|
+      t.string :uuid, :limit => 36
+
+      t.string :caller_number
+      t.string :caller_name
+      t.string :called_number
+      t.boolean :inbound, :null => false, :default => false
+      t.references :customer
+
+      t.datetime :create_time, :null => false
+      t.datetime :ring_time
+      t.datetime :answered_time
+      t.references :user
+
+      t.string :chan_name
+
+      t.string :end_reason
+      t.datetime :end_time
+    end
+    add_foreign_key(:calls, :customers)
+    add_foreign_key(:calls, :users)
+
+    remove_column :users, :incoming_phone_number
+    remove_column :users, :incoming_phone_name
+    remove_column :users, :incoming_phone_time
+  end
+
+  def self.down
+  end
+end
