433fbd821f59bba5f705c611ea38f87370edf3d8
Quinn Harris <quinn@qutek.net>
2011-07-23 19:27:41 -0600

    Missing

---
+++ config/initializers/wiked_pdf.rb
@@ -0,0 +1,8 @@
+WickedPdf.config = {
+  :page_size => 'Letter',
+  :margin => {
+    :top => '0.75in',
+    :bottom => '0.75in',
+    :left => '0.75in',
+    :right => '0.75in' },
+  :diable_javascript => true }
