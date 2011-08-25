6c717148171235fde0d919a1f210a87cac7de52f
Quinn Harris <quinn@qutek.net>
2011-08-25 10:01:27 -0600

    strip email

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -3,5 +3,10 @@ class EmailAddress < ActiveRecord::Base
 
   def self.main_column; 'address'; end
 
+  before_save :strip_address
+  def strip_address
+    self.address.strip!
+  end
+
   validates :address, :presence => true, :email => true
 end
