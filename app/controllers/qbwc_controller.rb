f63f7b2672bd96f063fed5be286b0dc6c9e90339
Quinn Harris <quinn@qutek.net>
2011-10-17 17:21:43 -0600

    Kludge qbwc?

--- app/controllers/qbwc_controller.rb
+++ app/controllers/qbwc_controller.rb
@@ -17,7 +17,7 @@ class QbwcController < ActionController::Base
     ActiveSupport::Deprecation.silence do
       message = "\n#{exception.class} (#{exception.message}):\n"
       message << exception.annoted_source_code.to_s if exception.respond_to?(:annoted_source_code)
-      message << "  " << application_trace(exception).join("\n  ")
+#      message << "  " << application_trace(exception).join("\n  ")
       logger.fatal("#{message}\n\n")
     end
   end
