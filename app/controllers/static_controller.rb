7e175d63869f8c55cd7b246ebc67ed8f577668f4
Quinn Harris <quinn@qutek.net>
2012-08-09 14:30:41 -0600

    Fix static title

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,8 +4,8 @@ class StaticController < ApplicationController
   before_filter :setup_context
 
   @@pages = [
-   ['About Us', 'about', 'About Us'],
-   ['Ordering', 'order', 'Order Information'],
+   ['About Us', 'about', 'About Mountain Xpress Promotions'],
+   ['Ordering', 'order', 'Order Information for Mountain Xpress Promotions'],
    ['Artwork', 'artwork', 'Artwork Requirements'],
    ['Decorations', 'decorations', 'Product Decorations'],
    ['Colors', 'color_chart', 'Pantone Color Chart'],
@@ -22,13 +22,14 @@ class StaticController < ApplicationController
   
   before_filter :set_title
   def set_title
-    record = (@@pages + @@hidden_pages).find { |p| p.last == params[:action] }
-    @title = record.first if record
+    record = (@@pages + @@hidden_pages).find { |p| p[1] == params[:action] }
+    puts "Title: #{@@pages.inspect}"
+    @title = record[2] if record
   end
 
   def sitemap
     headers['Content-Type'] = 'text/xml; charset=utf-8'
-    @links = @@pages.collect do |name, link|
+    @links = @@pages.collect do |name, link, title|
       { :loc => "http://www.mountainofpromos.com/static/#{link}" }
     end
     render :template => 'sitemaps/sitemap', :layout=>false
