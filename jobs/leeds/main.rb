ef02b7901f822873ee43f3af5d85068999802b9b
Quinn Harris <quinn@qutek.net>
2012-10-29 16:03:31 -0600

    Switch leeds

--- jobs/leeds/main.rb
+++ jobs/leeds/main.rb
@@ -4,7 +4,7 @@ require './import'
 class LeedsXLS < PolyXLS
   def initialize
     @src_urls = ['http://media.leedsworld.com/msfiles/downloads/WebDecorationMethodByItem.xls']
-    @src_urls += %w(USDcatalog USDMemorycatalog).collect do |name|
+    @src_urls += %w(USDMemorycatalog USDcatalog).collect do |name|
       "http://media.leedsworld.com/ms/?/excel/#{name}/EN"
     end
     @image_url = 'images.leedsworld.com'
