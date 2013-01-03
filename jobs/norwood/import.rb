cae13ec5d0913a8438bc9159637f4f033b34d9a2
Quinn Harris <quinn@mountainofpromos.com>
2013-01-03 08:40:11 -0700

    Norwood fixes

--- jobs/norwood/import.rb
+++ jobs/norwood/import.rb
@@ -1,3 +1,77 @@
+# Fix for norwood FTP parse
+class Net::FTP::List::Unix < Net::FTP::List::Parser
+
+  # Stolen straight from the ASF's commons Java FTP LIST parser library.
+  # http://svn.apache.org/repos/asf/commons/proper/net/trunk/src/java/org/apache/commons/net/ftp/
+  REGEXP = %r{
+([pbcdlfmpSs-])
+(((r|-)(w|-)([xsStTL-]))((r|-)(w|-)([xsStTL-]))((r|-)(w|-)([xsStTL-])))\+?\s+
+(?:(\d+)\s+)?
+(\S+)\s+
+(?:(\S+(?:\s\S+)*)\s+)?
+(?:\d+,\s+)?
+(\d+)\s+
+((?:\d+[-/]\d+[-/]\d+)|(?:\S+\s+\S+))\s+
+(\d+(?::\d+)?)\s+
+(\S*)(\s*.*)$
+}x
+# !!! Added $ to capture to end
+
+  ONE_YEAR = (60 * 60 * 24 * 365)
+
+  # Parse a Unix like FTP LIST entries.
+  def self.parse(raw)
+    match = REGEXP.match(raw) or return false # !!! Removed strip
+
+    dir, symlink, file, device = false, false, false, false
+    case match[1]
+      when /d/ then dir = true
+      when /l/ then symlink = true
+      when /[f-]/ then file = true
+      when /[psbc]/ then device = true
+    end
+    return false unless dir or symlink or file or device
+
+    # Don't match on rumpus (which looks very similar to unix)
+    return false if match[17].nil? and ((match[15].nil? and match[16].to_s == 'folder') or match[15].to_s == '0')
+
+    # TODO: Permissions, users, groups, date/time.
+    filesize = match[18].to_i
+    mtime_string = "#{match[19]} #{match[20]}"
+    mtime = Time.parse(mtime_string)
+
+    # Unix mtimes specify a 4 digit year unless the data is within the past 180
+    # days or so. Future dates always specify a 4 digit year.
+    #
+    # Since the above #parse call fills in today's date for missing date
+    # components, it can sometimes get the year wrong. To fix this, we make
+    # sure all mtimes without a 4 digit year are in the past.
+    if match[20] !~ /\d{4}/ && mtime > Time.now
+      mtime = Time.parse("#{mtime_string} #{mtime.year - 1}")
+    end
+
+    basename = match[21].strip
+
+    # filenames with spaces will end up in the last match
+    basename += match[22] unless match[22].nil?
+
+    # strip the symlink stuff we don't care about
+    basename.sub!(/\s+\->.+$/, '') if symlink
+
+    emit_entry(
+      raw,
+      :dir => dir,
+      :file => file,
+      :device => device,
+      :symlink => symlink,
+      :filesize => filesize,
+      :basename => basename,
+      :mtime => mtime
+    )
+  end
+end
+
+
 class NorwoodAll < GenericImport
   def initialize
     @year = (Date.today + 7).year
@@ -39,7 +113,13 @@ class NorwoodAll < GenericImport
     end
 
     # Get Image List
-    directory_list = %w(2012_Hardgoods_Hi_Res_Imprint_Images 2012_NPS3_Hi-Res_Images 2013_Calendars_Hi_Res_Imprint_Images 2012_Hardgoods_Hi_Res_Blank_Images 2013_Calendars_Hi_Res_Blank_Images 2012_Lifestyle_Images/2012_High_Res_Images).collect { |p| "Norwood 2012 Product Images/#{p}" }
+    directory_list = %w(
+      2012_NPS3_Hi-Res_Images
+      2013_Hardgoods_Hi_Res_Imprint_Images
+      2013_Hardgoods_Hi_Res_Blank_Images
+      2013_Lifestyle_Images/2013_High_Res_Images
+      2014_Calendars_Hi_Res_Imprint_Images
+      2014_Calendars_Hi_Res_Blank_Images ).collect { |p| "Norwood Product Images/#{p}" }
 
     @image_list = cache_marshal('Norwood_imagelist') do
       get_ftp_images({ :server => 'library.norwood.com',
@@ -114,7 +194,7 @@ class NorwoodCSV < GenericImport
   def parse_products
     puts "Reading: #{@file_name}"
 
-    CSV.foreach(File.join(JOBS_DATA_ROOT,@file_name), :headers => :first_row) do |row|
+    CSV.foreach(File.join(JOBS_DATA_ROOT,@file_name), :headers => :first_row, :encoding => 'windows-1251:utf-8') do |row|
       ProductDesc.apply(self) do |pd|
         pd.supplier_num = @supplier_num = row['Product ID']
         pd.name = row['Product Name']
@@ -126,7 +206,7 @@ class NorwoodCSV < GenericImport
         if sufix = @@sufixes[pd.supplier_categories.first]
           unless pd.name.downcase.include?(sufix.downcase)
             pd.name << " #{sufix}"
-            puts "Append: #{pd.supplier_num}: #{product_data['name']}"
+            puts "Append: #{pd.supplier_num}: #{pd.name}"
           end
         end
         
