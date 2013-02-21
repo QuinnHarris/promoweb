77c60fb495aefaa575c915d4b81f5fa9dc01457d
Quinn Harris <quinn@qutek.net>
2013-02-21 16:09:21 -0700

    Ash Sprint

--- jobs/ashcity/main.rb
+++ jobs/ashcity/main.rb
@@ -1,8 +1,7 @@
 require '../generic_import'
 require './import'
 
-#import = AshCityXLS.new(['US_ProductDataFile_SKUs.xls', 'SPRING 2012_USA_ProductDataFile_SKUs.xls'])
-import = AshCityXLS.new('USD_ProductDataFile_SKU.csv')
+import = AshCityXLS.new(['USD_ProductDataFile_SKU.csv', 'SPRING_USD_ProductDataFile_SKU.csv'])
 import.run_parse_cache
 import.run_transform
 import.run_apply_cache
