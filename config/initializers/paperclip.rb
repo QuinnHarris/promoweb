14b054bd7506ae16fab9ed8ed3b0b28ab9af6324
Quinn Harris <quinn@quinnharris.me>
2014-04-22 13:31:47 -0300

    Disable paperclip spoof to allow all file types

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,9 @@
+# Disable paperclip spoof detection so all file types are accepted
+require 'paperclip/media_type_spoof_detector'
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
